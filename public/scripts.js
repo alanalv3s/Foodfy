087d790cac2b5d29f90b6339bde128360260902a
alanams97 <alan_ams97@gmail.com>
2020-05-27 20:55:29 -0300

    refactor: Nunjucks and recipe page

--- public/scripts.js
+++
@@ -1,8 +0,0 @@
-const modalBase = document.querySelector('.modal-base')
-const cards = document.querySelectorAll('.card')
-
-for (let card of cards) {
-    card.addEventListener('click', () => modalBase.classList.add('active'))
-}
-
-document.querySelector('.close-modal').addEventListener('click', () => modalBase.classList.remove('active'))
