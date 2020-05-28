3c5c95d0fe37537e0619e22d68eb78a0c5550a1b
alanams97 <alan_ams97@gmail.com>
2020-05-28 20:50:03 -0300

    feat: Add browsersync

--- src/routes.js
+++ src/routes.js
@@ -4,14 +4,14 @@ const routes = express.Router()
 const recipes = require('./data')
 
 routes.get('/', (_, res) => res.render('index', { recipes }))
-routes.get('/receitas', (_, res) => res.render('recipes', { recipes }))
-routes.get('/receitas/:recipe_index', (req, res) => { 
+routes.get('/recipes', (_, res) => res.render('recipes', { recipes }))
+routes.get('/recipes/:recipe_index', (req, res) => { 
     const { recipe_index } = req.params  
     const recipe = recipes[recipe_index]
 
     res.render('recipe', { recipe })
 })
-routes.get('/sobre', (_, res) => res.render('about'))
+routes.get('/about', (_, res) => res.render('about'))
 
 
 module.exports = routes
